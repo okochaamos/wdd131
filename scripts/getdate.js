b3ad77aa27b6dd2c58d90bbcf5f377f270fded15
Okocha <oko23015@byui.edu>
2024-06-17 22:31:55 -0700

    commit

---
+++ scripts/getdate.js
@@ -0,0 +1,11 @@
+const today = new Date();
+
+const currentYear = document.querySelector("#currentYear");
+
+let lastModif = alert(document.lastModified);
+
+lastModif = documemt.querySelector("#lastModified");
+
+currentyear.innerHTML = 
+
+
