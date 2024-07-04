0b88d15003ff21732a76dde51ecbb53135f0cc95
Okocha <oko23015@byui.edu>
2024-07-04 10:50:31 -0700

    commit

---
+++ scripts/places.js
@@ -0,0 +1,15 @@
+var tempInCelcius = 27;
+var windSpeed = 5.4;
+
+temperature = parseFloat(tempInCelcius * 9/5) + 32;
+
+if(temperature > 50 && windSpeed > 4.8){
+    var windChill =
+    35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16;
+}else{
+    windChill = "N/A";
+}
+document.getElementById("wind-chill").innerHTML = `Windchill : ${Math.round(windChill)}°C`;
+
+
+
